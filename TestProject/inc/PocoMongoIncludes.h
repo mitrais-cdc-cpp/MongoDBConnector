1919772b04ad421f89cd53342f16fb64f3118d78
Thomas Rothe <eingefuchst@gmail.com>
2016-06-08 10:38:32 +0800

    basic refactor

---
+++ TestProject/inc/PocoMongoIncludes.h
@@ -0,0 +1,17 @@
+#ifndef MONGODAOLIB_POCOMONGOINCLUDES_H_
+#define MONGODAOLIB_POCOMONGOINCLUDES_H_
+
+#include "Poco/MongoDB/Connection.h"
+#include "Poco/MongoDB/Database.h"
+#include "Poco/MongoDB/InsertRequest.h"
+#include "Poco/MongoDB/UpdateRequest.h"
+#include "Poco/MongoDB/DeleteRequest.h"
+#include "Poco/MongoDB/QueryRequest.h"
+#include "Poco/MongoDB/MongoDB.h"
+
+#include "Poco/SharedPtr.h"
+#include "Poco/Net/NetException.h"
+
+#endif /* MONGODAOLIB_POCOMONGOINCLUDES_H_ */
+
+
