d74b382a82c77a7e734d67c2327a78058dfb56b8
Thomas Rothe <eingefuchst@gmail.com>
2016-06-10 10:13:04 +0800

    removed temporary cmake stuff

--- TestProject/cmake/CMakeFiles/MongoDaoLib.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "libMongoDaoLib.pdb"
-  "libMongoDaoLib.so"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/MongoDaoLib.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
